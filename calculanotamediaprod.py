f02d394de6a9588d8395212e63a4a3e2d4de8991
Yuji <yuji.niitsuma@zettaes.com.br>
2024-09-19 20:46:27 -0300

    Essa é a versão inicial do código-fonte calculanotamediaprod.py. Esse programa calcula a média de nota enviada pelos clientes que já adquiriram os produtos. Serão enviadas 3 notas pela plataforma de ecommerce.

---
+++ calculanotamediaprod.py
@@ -0,0 +1,19 @@
+titulo = "Calcula o valor médio de avaliação do produto"
+print(f'{titulo:^50}')
+
+nota1 = int(input("Digite a primeira nota avaliada do produto pelo cliente: "))
+
+nota2 = int(input("Digite a segunda nota avaliada do produto pelo cliente: "))
+
+nota3 = int(input("Digite a terceira nota avaliada do produto pelo cliente: "))
+
+media = (float(nota1) + float(nota2) + float(nota3))/3
+
+print("A média alcançada foi: %s" % media)
+
+if media > 6:
+    print("Parabéns, produto adequado aos clientes!")
+else:
+    print("Que pena, esse produto inadequado e com muitas reclamações!")
+	
+	   
